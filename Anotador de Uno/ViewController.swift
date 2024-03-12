9dfe64827a476c564fe95f97ff8005730d98d4dd
Nicolás Fiore <nicolasfiore@Mac-mini-de-Nicolas.local>
2024-03-12 12:43:12 -0300

    cambio de la logica en la app

--- Anotador de Uno/ViewController.swift
+++ Anotador de Uno/ViewController.swift
@@ -31,12 +31,124 @@ class ViewController: UIViewController {
         let cantidad = Int(stepperJuadores.value)
         cantidadJugadores.text = String(cantidad)
         
+        
+        switch Int(stepperJuadores.value){
+        case 2:
+            cantidadDeJugadores.removeAll()
+            for x in 1...2 {
+                cantidadDeJugadores.append(x)
+                print(cantidadDeJugadores)
+                tableView.reloadData()
+            }
+            break
+        case 3:
+            cantidadDeJugadores.removeAll()
+            for x in 1...3 {
+                cantidadDeJugadores.append(x)
+                print(cantidadDeJugadores)
+                tableView.reloadData()
+            }
+            break
+        case 4:
+            cantidadDeJugadores.removeAll()
+            for x in 1...4 {
+                cantidadDeJugadores.append(x)
+                print(cantidadDeJugadores)
+                tableView.reloadData()
+            }
+            break
+        case 5:
+            cantidadDeJugadores.removeAll()
+            for x in 1...5 {
+                cantidadDeJugadores.append(x)
+                print(cantidadDeJugadores)
+                tableView.reloadData()
+            }
+            break
+        case 6:
+            cantidadDeJugadores.removeAll()
+            for x in 1...6 {
+                cantidadDeJugadores.append(x)
+                print(cantidadDeJugadores)
+                tableView.reloadData()
+            }
+            break
+        case 7:
+            cantidadDeJugadores.removeAll()
+            for x in 1...7 {
+                cantidadDeJugadores.append(x)
+                print(cantidadDeJugadores)
+                tableView.reloadData()
+            }
+            break
+        case 8:
+            cantidadDeJugadores.removeAll()
+            for x in 1...8 {
+                cantidadDeJugadores.append(x)
+                print(cantidadDeJugadores)
+                tableView.reloadData()
+            }
+            break
+        case 9:
+            cantidadDeJugadores.removeAll()
+            for x in 1...9 {
+                cantidadDeJugadores.append(x)
+                print(cantidadDeJugadores)
+                tableView.reloadData()
+            }
+            break
+        case 10:
+            cantidadDeJugadores.removeAll()
+            for x in 1...10 {
+                cantidadDeJugadores.append(x)
+                print(cantidadDeJugadores)
+                tableView.reloadData()
+            }
+            break
+        case 11:
+            cantidadDeJugadores.removeAll()
+            for x in 1...11 {
+                cantidadDeJugadores.append(x)
+                print(cantidadDeJugadores)
+                tableView.reloadData()
+            }
+            break
+        case 12:
+            cantidadDeJugadores.removeAll()
+            for x in 1...12 {
+                cantidadDeJugadores.append(x)
+                print(cantidadDeJugadores)
+                tableView.reloadData()
+            }
+            break
+        case 13:
+            cantidadDeJugadores.removeAll()
+            for x in 1...13 {
+                cantidadDeJugadores.append(x)
+                print(cantidadDeJugadores)
+                tableView.reloadData()
+            }
+            break
+        case 14:
+            cantidadDeJugadores.removeAll()
+            for x in 1...14 {
+                cantidadDeJugadores.append(x)
+                print(cantidadDeJugadores)
+                tableView.reloadData()
+            }
+            break
+        default:
+            print("ERROR")
+        }
+        
+        /*
         if stepperJuadores.value > 2 {
             cantidadDeJugadores.append(1)
             tableView.reloadData()
             print(cantidadDeJugadores)
             print("Cantidad:\(cantidad)")
-        }
+        }*/
+        
     }
 }
 
